b131b87d0a3499456ab2424f5882974ccd65b70c
Pastas <92827346+P4ST4S@users.noreply.github.com>
2024-09-16 15:54:17 +0200

    Dev (#11)
    * feat: update README.md with project description and dependencies
    
    * feat: update README.md with project description, dependencies, and installation instructions
    
    * chore: Update bcrypt dependency to bcryptjs
    
    * chore: Update .dockerignore, .gitignore, Dockerfile, package.json, and docker-compose.yml files
    
    * chore: Remove @types/bcrypt dependency and update to bcryptjs
    
    * chore: Remove docker-compose.yml file
    
    * chore(deps): remove @types/bcrypt and mark dependencies as dev
    
    * chore: Update dependencies and add Swagger API documentation
    
    * feat(auth): Add Swagger API documentation to User model
    
    This commit adds Swagger API documentation to the User model in the auth module. It includes annotations for the email, password, and username properties, providing examples and descriptions for each.

--- src/auth/auth.model.ts
+++ src/auth/auth.model.ts
@@ -1,7 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Prisma } from '@prisma/client';
 
 export class User implements Prisma.UserUncheckedCreateInput {
+  @ApiProperty({
+    example: 'user@example.com',
+    description: 'Email address of the user',
+    uniqueItems: true,
+  })
   email: string;
+
+  @ApiProperty({
+    example: 'strong_password_123',
+    description: 'Hashed password for the user',
+  })
   password: string;
+
+  @ApiProperty({
+    example: 'username123',
+    description: 'Unique username chosen by the user',
+    uniqueItems: true,
+  })
   username: string;
 }
