0928d2b1743bb61b63ac216a609931f2bf0db05f
P4ST4S <antoine.rospars@epitech.eu>
2024-10-09 13:12:12 +0200

    tests(auth): add tests for all get

--- src/auth/tests/auth.service.spec.ts
+++ src/auth/tests/auth.service.spec.ts
@@ -32,6 +32,7 @@ describe('AuthService', () => {
             user: {
               findUnique: jest.fn(),
               create: jest.fn(),
+              findMany: jest.fn(),
             },
           },
         },
@@ -94,6 +95,106 @@ describe('AuthService', () => {
       const result = await service.signUp(signUpInput);
 
       expect(result).toEqual({ access_token: 'mockedJwtToken' });
+    });
+  });
+
+  describe('getAllUsers', () => {
+    it('should return an array of users', async () => {
+      const mockUsers = [
+        {
+          id: '1',
+          email: 'user1@test.com',
+          password: 'hashedPassword1',
+          username: 'user1',
+          accessToken: 'accessToken1',
+          refreshToken: 'refreshToken1',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: '2',
+          email: 'user2@test.com',
+          password: 'hashedPassword2',
+          username: 'user2',
+          accessToken: 'accessToken2',
+          refreshToken: 'refreshToken2',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ];
+
+      jest.spyOn(prisma.user, 'findMany').mockResolvedValue(mockUsers);
+
+      const result = await service.getAllUsers();
+
+      expect(result).toEqual(mockUsers);
+      expect(prisma.user.findMany).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('getUserById', () => {
+    it('should return a user when found by ID', async () => {
+      jest.spyOn(prisma.user, 'findUnique').mockResolvedValue(mockUser);
+
+      const result = await service.getUserById('1');
+      expect(result).toEqual(mockUser);
+      expect(prisma.user.findUnique).toHaveBeenCalledWith({
+        where: { id: '1' },
+      });
+    });
+
+    it('should return null if no user is found by ID', async () => {
+      jest.spyOn(prisma.user, 'findUnique').mockResolvedValue(null);
+
+      const result = await service.getUserById('999');
+      expect(result).toBeNull();
+      expect(prisma.user.findUnique).toHaveBeenCalledWith({
+        where: { id: '999' },
+      });
+    });
+  });
+
+  describe('getUserByEmail', () => {
+    it('should return a user when found by email', async () => {
+      jest.spyOn(prisma.user, 'findUnique').mockResolvedValue(mockUser);
+
+      const result = await service.getUserByEmail('test@test.com');
+      expect(result).toEqual(mockUser);
+      expect(prisma.user.findUnique).toHaveBeenCalledWith({
+        where: { email: 'test@test.com' },
+      });
+    });
+
+    it('should return null if no user is found by email', async () => {
+      jest.spyOn(prisma.user, 'findUnique').mockResolvedValue(null);
+
+      const result = await service.getUserByEmail('notfound@test.com');
+      expect(result).toBeNull();
+      expect(prisma.user.findUnique).toHaveBeenCalledWith({
+        where: { email: 'notfound@test.com' },
+      });
+    });
+  });
+
+  describe('getUserByUsername', () => {
+    it('should return a user when found by username', async () => {
+      jest.spyOn(prisma.user, 'findUnique').mockResolvedValue(mockUser);
+
+      const result = await service.getUserByUsername('testuser');
+      expect(result).toEqual(mockUser);
+      expect(prisma.user.findUnique).toHaveBeenCalledWith({
+        where: { username: 'testuser' },
+      });
+    });
+
+    it('should return null if no user is found by username', async () => {
+      jest.spyOn(prisma.user, 'findUnique').mockResolvedValue(null);
+
+      const result = await service.getUserByUsername('notfounduser');
+      expect(result).toBeNull();
+      expect(prisma.user.findUnique).toHaveBeenCalledWith({
+        where: { username: 'notfounduser' },
+      });
     });
   });
 
